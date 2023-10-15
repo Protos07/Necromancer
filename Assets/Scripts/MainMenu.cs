048f5fe19fa9aa836df3b980f93faf06cfb6c128
Denis <116489846+Protos07@users.noreply.github.com>
2023-10-15 14:27:34 +0300

    Revert "Merge pull request #5 from Protos07/InClass"
    This reverts commit 4ea67e55f3448d2d0021a54e612be2b7aec83293, reversing
    changes made to 311cbe24d7e2c3ca82c6a472b2a83eb1ab24020c.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject setting;
+    public GameObject exit_panel;
+    public GameObject main;
+
+    public void EnterSetting()
+    {
+        menu.SetActive(false);
+        setting.SetActive(true);
+    }
+    public void ExitSetting()
+    {
+        setting.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
+
+    public void EnterMenuInGame()
+    {
+        main.SetActive(false);
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ExitMenuInGame()
+    {
+        main.SetActive(true);
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void ExitInGame()
+    {
+        menu.SetActive(false);
+        exit_panel.SetActive(true);
+    }
+
+    public void ExitInGameCancel()
+    {
+        menu.SetActive(true);
+        exit_panel.SetActive(false);
+    }
+}
