c6c065395dcf81b51c98093c72602dd353edb1c1
Denis <116489846+Protos07@users.noreply.github.com>
2023-10-16 12:54:55 +0300

    fixed ui

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyHealth : Health
 {
-
+    private Collider2D col;
+    public void Start()
+    {
+        Enemy enemy_cast = GetComponent<Enemy>();
+    }
     public void Update()
     {        
         health_bar_fill.value = Mathf.Lerp(health_bar_fill.value, health_bar.value, speed);
              
         if (health_bar.value == 0)
         {
-            Debug.Log("You killed him!");
-            Destroy(this.gameObject);
+            Debug.Log("You killed him!");           
+
         }
     }
 }
---
+++ Assets/Scripts/Resurrection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resurrection : Enemy
+{
+    public GameObject resurrection_object;
+
+
+
+
+}
