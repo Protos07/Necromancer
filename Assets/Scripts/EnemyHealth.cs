cbd6c9749efa3e7aaa5da3fb3af33efb6be6f8e5
Denis <116489846+Protos07@users.noreply.github.com>
2023-10-26 19:24:15 +0300

    try fix

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class EnemyHealth : Health
 {
-<<<<<<< HEAD
     private Collider2D col;
     public Enemy enemy_cast;
     public Transform player;
@@ -14,23 +13,15 @@ public class EnemyHealth : Health
         col = GetComponent<Collider2D>();
 
     }
-=======
-
->>>>>>> parent of c6c0653 (fixed ui)
     public void Update()
     {        
         health_bar_fill.value = Mathf.Lerp(health_bar_fill.value, health_bar.value, speed);
              
         if (health_bar.value == 0)
         {
-<<<<<<< HEAD
             Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), col);
             enemy_cast.enabled = false;
             
-=======
-            Debug.Log("You killed him!");
-            Destroy(this.gameObject);
->>>>>>> parent of c6c0653 (fixed ui)
         }
     }
 }
